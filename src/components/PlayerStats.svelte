2f12eb73f5da6b19ac8c0504bca4aa679339be66
Mike Peiman <mikepeiman@gmail.com>
2019-10-16 18:13:38 -0700

    Improved styling on detail panels

--- src/components/PlayerStats.svelte
+++ src/components/PlayerStats.svelte
@@ -5,22 +5,28 @@ export let turns, name, player
 
 <div class="player-stats">
 
-<h1>Player Stats</h1>
-<span>name: {player.name}</span>
-<span>turns: {player.turns}</span>
-<span>player: {player}</span>
+<h1>Player</h1>
+<span class="data-stats">Name: {player.name}</span>
+<span class="data-stats">Turns: {player.turns}</span>
 
 </div>
 
 <style lang=scss>
 
+  .data-stats {
+    background: linear-gradient(90deg,rgba(255,100,155,0.75), rgba(155,100,255,0.75));
+    padding: 0.15rem .35rem;
+    margin: 1rem;
+    line-height: 2rem;
+  }
+
 .player-stats {
   position: absolute;
-  width: 100px;
-  height: 300px;
+  width: 20ch;
+  // height: 300px;
   top: 0;
-  right: 120px;
-  background: rgba(0,0,0,0.75);
+  right: 30ch;
+  background: #333;
   color: white;
   padding: 1rem;
   border: 5px solid black;
--- src/components/ShipStats.svelte
+++ src/components/ShipStats.svelte
@@ -5,22 +5,28 @@ export let ship, currentShip
 
 <div class="ship-stats">
 
-<h1>ship Stats</h1>
-<span>name: {currentShip.name}</span>
-<span>moves: {currentShip.moves}</span>
-<span>currentShip: {currentShip}</span>
+<h1>Ship</h1>
+<span class="data-stats">Type: {currentShip.name}</span>
+<span class="data-stats">Moves: {currentShip.moves}</span>
 
 </div>
 
 <style lang=scss>
 
+.data-stats {
+  background: linear-gradient(90deg,rgba(255,100,155,0.75), rgba(155,100,255,0.75));
+  padding: 0.15rem .35rem;
+  margin: 1rem;
+  line-height: 2rem;
+}
+
 .ship-stats {
   position: absolute;
-  width: 100px;
-  height: 300px;
+  width: 20ch;
+  // height: 300px;
   top: 0;
-  right: 10px;
-  background: rgba(0,0,0,0.75);
+  right: 1rem;
+  background: #333;
   color: white;
   padding: 1rem;
   border: 5px solid black;
